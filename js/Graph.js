b1d9d5676355d2a98baf2a145d263f216357a047
Karan gandhi <karangandhi.programming@gmail.com>
2020-01-19 23:26:36 +0530

    added the randomization of data of a array in the graph class

--- js/Graph.js
+++ js/Graph.js
@@ -3,4 +3,10 @@ class Graph {
         this.array = array;
         this.size = size;
     }
+
+    randomise() {
+        for (let i = 0; i > size; i++) {
+            this.array[i] = Math.floor(Math.random() * 1000 + 10);
+        }
+    }
 }
