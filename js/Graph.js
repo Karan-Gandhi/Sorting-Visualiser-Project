e5020bbda2d2ec13ca805595bca316e18aa4f9c7
Karan Gandhi <51285551+Karan-Gandhi@users.noreply.github.com>
2020-01-20 19:44:11 +0530

    Revert "Visualise data in array"

--- js/Graph.js
+++ js/Graph.js
@@ -1,47 +1,12 @@
 class Graph {
-    constructor(size, width, height, root) {
-        this.array = [];
-        for (let i = 0; i < size; i++) {
-            this.array.push(0);
-        }
+    constructor(array, size) {
+        this.array = array;
         this.size = size;
-        this.dimentions = {
-            w: width,
-            h: height
-        };
-        this.root = root;
     }
 
     randomise() {
-        for (let i = 0; i < this.size; i++) {
-            this.array[i] = Math.floor(Math.random() * (1000 - 50) + 50);
+        for (let i = 0; i > size; i++) {
+            this.array[i] = Math.floor(Math.random() * 1000 + 10);
         }
     }
-
-    showGraph() {
-        let barWidth = this.dimentions.w / this.array.length - 1;
-        for (let i = 0; i < this.array.length; i++) {
-            const bar = createElement("div", "bar");
-            bar.style.position = "absolute";
-            bar.style.left = i * barWidth + i + "px";
-            bar.style.bottom = 0 + "px";
-            bar.style.width = barWidth + "px";
-            bar.style.height = (this.array[i] / 1000) * this.dimentions.h + "px";
-            this.root.append(bar);
-        }
-        for (var i = 30; i < this.dimentions.h; i += 30) {
-            const line = createElement("div", "tick");
-            line.innerHTML = i;
-            line.style.position = "absolute";
-            line.style.bottom = i + "px";
-            line.style.left = -25 + "px";
-            this.root.append(line);
-        }
-    }
-}
-
-function createElement(tag, id) {
-    const elt = document.createElement(tag);
-    elt.id = id;
-    return elt;
 }
--- js/main.js
+++ js/main.js
@@ -1,7 +1,1 @@
-let graph = null;
-
-window.onload = () => {
-    graph = new Graph(500, document.getElementById("root").offsetWidth, document.getElementById("root").offsetHeight - 100, document.getElementById("root"));
-    graph.randomise();
-    graph.showGraph();
-};
+const graph = new Graph([], 10);
