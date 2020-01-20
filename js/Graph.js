49b456ab38fb59d2d50e6084a8a2c12c81653377
Karan gandhi <karangandhi.programming@gmail.com>
2020-01-20 19:11:29 +0530

    created the bars

--- js/Graph.js
+++ js/Graph.js
@@ -1,12 +1,39 @@
 class Graph {
-    constructor(array, size) {
-        this.array = array;
+    constructor(size, width, height, root) {
+        this.array = [];
+        for (let i = 0; i < size; i++) {
+            this.array.push(0);
+        }
         this.size = size;
+        this.dimentions = {
+            w: width,
+            h: height
+        };
+        this.root = root;
     }
 
     randomise() {
-        for (let i = 0; i > size; i++) {
-            this.array[i] = Math.floor(Math.random() * 1000 + 10);
+        for (let i = 0; i < this.size; i++) {
+            this.array[i] = Math.floor(Math.random() * (1000 - 10) + 10);
         }
     }
+
+    show() {
+        let barWidth = this.dimentions.w / this.array.length - 1;
+        for (let i = 0; i < this.array.length; i++) {
+            let bar = createElement("div", "bar");
+            bar.style.position = "absolute";
+            bar.style.left = i * barWidth + i + "px";
+            bar.style.bottom = 0 + "px";
+            bar.style.width = barWidth + "px";
+            bar.style.height = (this.array[i] / 1000) * this.dimentions.h + "px";
+            this.root.append(bar);
+        }
+    }
+}
+
+function createElement(tag, id) {
+    const elt = document.createElement(tag);
+    elt.id = id;
+    return elt;
 }
--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-const graph = new Graph([], 10);
+let graph = null;
+
+window.onload = () => {
+    graph = new Graph(10, document.getElementById("root").offsetWidth, document.getElementById("root").offsetHeight - 100, document.getElementById("root"));
+    graph.randomise();
+    graph.show();
+};
