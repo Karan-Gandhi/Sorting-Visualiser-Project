56ca02fbf90df9e337af39218ea735549214c53e
Karan gandhi <karangandhi.programming@gmail.com>
2020-01-22 19:46:49 +0530

    Impelmented the bubble sort algorithm

--- js/algorithms/BubbleSort.js
+++ js/algorithms/BubbleSort.js
@@ -1,1 +1,20 @@
-function BubbleSort() {}
+async function BubbleSort(graph) {
+    let arr = graph.array;
+    let n = arr.length;
+
+    for (let i = 0; i < n; i++) {
+        for (let x = 0; x < n; x++) {
+            if (arr[x] > arr[x + 1]) {
+                graph.compare(x, x + 1);
+                graph.swap(x, x + 1);
+                await sleep(1);
+                graph.showGraph();
+                graph.uncompare(x, x + 1);
+            }
+        }
+    }
+}
+
+async function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
